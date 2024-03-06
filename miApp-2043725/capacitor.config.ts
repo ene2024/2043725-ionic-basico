57e9b8c44c7c3c5fa8c99ef33930b3c0e4729d5d
Leonardo Pérez Lázaro <leonardo.perezlazaro@gmail.com>
2024-03-05 19:43:17 -0600

    Practica 6

---
+++ miApp-2043725/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miApp-2043725',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
